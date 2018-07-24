43e0cb192721f642790cde93adf283bb936596fb
Hannah Masila <hannahmasila@gmail.com>
2018-07-24 11:39:51 +0300

    now add the websocket logic

---
+++ web_socket_thread.py
@@ -0,0 +1,262 @@
+import sys
+import websocket
+import threading
+import traceback
+import ssl
+from time import sleep
+import json
+import decimal
+import logging
+import settings
+from future.standard_library import hooks
+with hooks():  # Python 2/3 compat
+    from urllib.parse import urlparse, urlunparse
+
+
+# Connects to BitMEX websocket for streaming realtime data.
+# The Marketmaker still interacts with this as if it were a REST Endpoint, but now it can get
+# much more realtime data without heavily polling the API.
+#
+# The Websocket offers a bunch of data as raw properties right on the object.
+# On connect, it synchronously asks for a push of all this data then returns.
+# Right after, the MM can start using its data. It will be updated in realtime, so the MM can
+# poll as often as it wants.
+class BitMEXWebsocket():
+
+    # Don't grow a table larger than this amount. Helps cap memory usage.
+    MAX_TABLE_LEN = 200
+
+    def __init__(self):
+        self.logger = logging.getLogger('root')
+        self.__reset()
+
+    def __del__(self):
+        self.exit()
+
+    def connect(self, endpoint="", symbol="XBTUSD"):
+        '''Connect to the websocket and initialize data stores.'''
+
+        self.logger.debug("Connecting WebSocket.")
+        self.symbol = symbol
+
+        # We can subscribe right in the connection querystring, so let's build that.
+        # Subscribe to all pertinent endpoints
+        subscriptions = [sub + ':' + symbol for sub in ["quote", "trade"]]
+        subscriptions += ["instrument"]  # We want all of them
+        
+        # Get WS URL and connect.
+        urlParts = list(urlparse(endpoint))
+        urlParts[0] = urlParts[0].replace('http', 'ws')
+        urlParts[2] = "/realtime?subscribe=" + ",".join(subscriptions)
+        wsURL = urlunparse(urlParts)
+        self.logger.info("Connecting to %s" % wsURL)
+        self.__connect(wsURL)
+        self.logger.info('Connected to WS. Waiting for data images, this may take a moment...')
+
+        # Connected. Wait for partials
+        self.__wait_for_symbol(symbol)
+        self.logger.info('Got all market data. Starting.')
+
+    #
+    # Data methods
+    #
+    def get_instrument(self, symbol):
+        instruments = self.data['instrument']
+        matchingInstruments = [i for i in instruments if i['symbol'] == symbol]
+        if len(matchingInstruments) == 0:
+            raise Exception("Unable to find instrument or index with symbol: " + symbol)
+        instrument = matchingInstruments[0]
+        # Turn the 'tickSize' into 'tickLog' for use in rounding
+        # http://stackoverflow.com/a/6190291/832202
+        instrument['tickLog'] = decimal.Decimal(str(instrument['tickSize'])).as_tuple().exponent * -1
+        return instrument
+
+    #
+    # Lifecycle methods
+    #
+    def error(self, err):
+        self._error = err
+        self.logger.error(err)
+        self.exit()
+
+    def exit(self):
+        self.exited = True
+        self.ws.close()
+
+    #
+    # Private methods
+    #
+
+    def __connect(self, wsURL):
+        '''Connect to the websocket in a thread.'''
+        self.logger.debug("Starting thread")
+
+        ssl_defaults = ssl.get_default_verify_paths()
+        sslopt_ca_certs = {'ca_certs': ssl_defaults.cafile}
+        self.ws = websocket.WebSocketApp(wsURL,
+                                         on_message=self.__on_message,
+                                         on_close=self.__on_close,
+                                         on_open=self.__on_open,
+                                         on_error=self.__on_error,
+                                         header=[]
+                                         )
+
+        setup_custom_logger('websocket', log_level=settings.LOG_LEVEL)
+        self.wst = threading.Thread(target=lambda: self.ws.run_forever(sslopt=sslopt_ca_certs))
+        self.wst.daemon = True
+        self.wst.start()
+        self.logger.info("Started thread")
+
+        # Wait for connect before continuing
+        conn_timeout = 5
+        while (not self.ws.sock or not self.ws.sock.connected) and conn_timeout and not self._error:
+            sleep(1)
+            conn_timeout -= 1
+
+        if not conn_timeout or self._error:
+            self.logger.error("Couldn't connect to WS! Exiting.")
+            self.exit()
+            sys.exit(1)
+
+    def __wait_for_symbol(self, symbol):
+        '''On subscribe, this data will come down. Wait for it.'''
+        while not {'instrument', 'trade', 'quote'} <= set(self.data):
+        	sleep(0.1)
+
+    def __on_message(self, ws, message):
+        '''Handler for parsing WS messages.'''
+        message = json.loads(message)
+        self.logger.debug(json.dumps(message))
+
+        table = message['table'] if 'table' in message else None
+        action = message['action'] if 'action' in message else None
+        try:
+            if 'subscribe' in message:
+                if message['success']:
+                    self.logger.debug("Subscribed to %s." % message['subscribe'])
+                else:
+                    self.error("Unable to subscribe to %s. Error: \"%s\" Please check and restart." %
+                               (message['request']['args'][0], message['error']))
+            elif 'status' in message:
+                if message['status'] == 400:
+                    self.error(message['error'])
+                if message['status'] == 401:
+                    self.error("API Key incorrect, please check and restart.")
+            elif action:
+
+                if table not in self.data:
+                    self.data[table] = []
+
+                if table not in self.keys:
+                    self.keys[table] = []
+
+                # There are four possible actions from the WS:
+                # 'partial' - full table image
+                # 'insert'  - new row
+                # 'update'  - update row
+                # 'delete'  - delete row
+                if action == 'partial':
+                    self.logger.debug("%s: partial" % table)
+                    self.data[table] += message['data']
+                    # Keys are communicated on partials to let you know how to uniquely identify
+                    # an item. We use it for updates.
+                    self.keys[table] = message['keys']
+                elif action == 'insert':
+                    self.logger.debug('%s: inserting %s' % (table, message['data']))
+                    self.data[table] += message['data']
+
+                    # Limit the max length of the table to avoid excessive memory usage.
+                    # Don't trim orders because we'll lose valuable state if we do.
+                    if table not in ['order', 'orderBookL2'] and len(self.data[table]) > BitMEXWebsocket.MAX_TABLE_LEN:
+                        self.data[table] = self.data[table][(BitMEXWebsocket.MAX_TABLE_LEN // 2):]
+
+                elif action == 'update':
+                    self.logger.debug('%s: updating %s' % (table, message['data']))
+                    # Locate the item in the collection and update it.
+                    for updateData in message['data']:
+                        item = findItemByKeys(self.keys[table], self.data[table], updateData)
+                        if not item:
+                            continue  # No item found to update. Could happen before push
+
+                        # Log executions
+                        if table == 'order':
+                            is_canceled = 'ordStatus' in updateData and updateData['ordStatus'] == 'Canceled'
+                            if 'cumQty' in updateData and not is_canceled:
+                                contExecuted = updateData['cumQty'] - item['cumQty']
+                                if contExecuted > 0:
+                                    instrument = self.get_instrument(item['symbol'])
+                                    self.logger.info("Execution: %s %d Contracts of %s at %.*f" %
+                                             (item['side'], contExecuted, item['symbol'],
+                                              instrument['tickLog'], item['price']))
+
+                        # Update this item.
+                        item.update(updateData)
+
+                        # Remove canceled / filled orders
+                        if table == 'order' and item['leavesQty'] <= 0:
+                            self.data[table].remove(item)
+
+                elif action == 'delete':
+                    self.logger.debug('%s: deleting %s' % (table, message['data']))
+                    # Locate the item in the collection and remove it.
+                    for deleteData in message['data']:
+                        item = findItemByKeys(self.keys[table], self.data[table], deleteData)
+                        self.data[table].remove(item)
+                else:
+                    raise Exception("Unknown action: %s" % action)
+        except:
+            self.logger.error(traceback.format_exc())
+ 
+    def __on_open(self, ws):
+        self.logger.debug("Websocket Opened.")
+
+    def __on_close(self, ws):
+        self.logger.info('Websocket Closed')
+        self.exit()
+
+    def __on_error(self, ws, error):
+        if not self.exited:
+            self.error(error)
+
+    def __reset(self):
+        self.data = {}
+        self.keys = {}
+        self.exited = False
+        self._error = None
+
+def setup_custom_logger(name, log_level=settings.LOG_LEVEL):
+    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(log_level)
+    logger.addHandler(handler)
+    return logger
+
+def findItemByKeys(keys, table, matchData):
+	for item in table:
+	    matched = True
+	    for key in keys:
+	        if item[key] != matchData[key]:
+	            matched = False
+	    if matched:
+	        return item
+
+
+if __name__ == "__main__":
+    # create console handler and set level to debug
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    # create formatter
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    # add formatter to ch
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    ws = BitMEXWebsocket()
+    ws.logger = logger
+    ws.connect(settings.BASE_URL)
+    while(ws.ws.sock.connected):
+        sleep(1)
